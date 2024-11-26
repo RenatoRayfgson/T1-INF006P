1dd5a531893186f28669dd0d4b837bf13af9ced0
Renato Rayfgson <rrayfgson@gmail.com>
2024-11-25 23:02:42 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+//Equipe: Renato Rayfgson Bittencourt(20241160004) e Anna Luisa(INSERIR NUMERO DE MATRÍCULA)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    return 0;
+}
