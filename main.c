3cd46128bbd006825ee1c985da96ed7aaf7d9ac0
Renato Rayfgson <rrayfgson@gmail.com>
2024-12-03 13:30:02 -0300

    adicionado arquivo de teste

--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 #include <string.h>
 #include <math.h>
 
+struct Pontos{
+    int x;
+    int y;
+    float distancia;
+};
+
 int main(){
+    
+    FILE *fp_in = fopen("L0Q1.in","r");
+    FILE *fp_out = fopen("L0Q1.out","w");
+
+    struct Pontos listaPontos[1000];
+
     return 0;
 }
