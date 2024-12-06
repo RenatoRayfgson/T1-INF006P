f30ab259c32778920969234ab13106894e32ae03
Renato Rayfgson <rrayfgson@gmail.com>
2024-12-06 14:10:59 -0300

    finalizado

--- Q1.c
+++ Q1.c
@@ -10,17 +10,34 @@ typedef struct Pontos{
     float distancia;
 }Pontos;
 
-void bubbleSort(Pontos p[], int k){
+void bubbleSort(Pontos ponto[], int k){//"Ai, mas bubbleSort é ruim demais", eu sei, Dihego, mas eu sei de cor e ele é um método de organização estável, juro que não vai comprometer nesse código <3
     Pontos temp;
     for(int i=0; i<k; i++){
         for(int j=0; j<k-1; j++){
-            if(p[j].distancia>p[j+1].distancia){
-                temp = p[j];
-                p[j] = p[j+1];
-                p[j+1]=temp;
+            if(ponto[j].distancia>ponto[j+1].distancia){
+                temp = ponto[j];
+                ponto[j] = ponto[j+1];
+                ponto[j+1]=temp;
             }
         }
     }
+}
+
+void criarSaida(FILE *fp_out, Pontos ponto[], int contador, float distanciaTotal, float distanciaAtalho, int contagemDeLinhas){ //Pra deixar menos poluído fiz uma função q cria a saída direto
+    char saida[1000]="";
+    int c=0;
+
+    sprintf(saida, (contagemDeLinhas == 0) ? "points" : "\npoints");
+    
+    while(c<contador){
+        sprintf(saida, "%s (%d,%d)", saida, ponto[c].x, ponto[c].y); //Essa linha pode ter ficado um pouco confusa, mas é só um while q tá concatenando as coordenadas com a saída anterior, acredito q vá funcionar
+        c++;
+    }
+
+    sprintf(saida, "%s distance %.2f", saida, distanciaTotal);
+    sprintf(saida, "%s shortcut %.2f", saida, distanciaAtalho);
+
+    fputs(saida, fp_out);
 }
 
 int main(){
@@ -31,17 +48,16 @@ int main(){
     char linha[1000];
     int contagemDeLinhas=0;
     char espaco[]=" ";
+    
     if(fp_in == NULL || fp_out == NULL){
         printf("Nao foi possivel abrir os arquivos!");
         return EXIT_FAILURE;
     }
 
     while(fgets(linha, sizeof(linha), fp_in) != NULL){
-        Pontos p[200];
+        Pontos ponto[200];
         int contador=0;
-        float distanciaTotal=0, distanciaAtalho=0;
-        char saida[1000];
-        int c=0;
+        float distanciaTotal=0, distanciaAtalho=0;       
         char *pedaco = strtok(linha, espaco);
 
         if(strcmp(pedaco, "points") == 0){ //Isso é capricho meu, tem jeito mais fácil de fazer
@@ -51,34 +67,26 @@ int main(){
             if(pedaco[0] == '('){
                 int x,y;
                 if(sscanf(pedaco, "(%d,%d)", &x, &y) == 2){ //Eu não sei se essa função vai funcionar, se nao funcionar a gente vai ter de arrumar outra forma
-                    p[contador].x = x; //Ok, sscanf nao pode ser atribuído direto ele meio q faz o atoi, criei x e y por isso
-                    p[contador].y = y;
-                    p[contador].distancia = sqrt((float)pow(x,2) + pow(y,2));
+                    ponto[contador].x = x; //Ok, sscanf nao pode ser atribuído direto no struct, ele meio q faz o atoi, criei x e y por isso
+                    ponto[contador].y = y;
+                    ponto[contador].distancia = sqrt((float)pow(x,2) + pow(y,2));
                     contador++;
                 }
             }
             pedaco = strtok(NULL, espaco);
         }
+        distanciaAtalho = sqrt((float)(pow(ponto[contador-1].x - ponto[0].x, 2)) + (pow(ponto[contador-1].y - ponto[0].y, 2)));
+        
         for(int i=0; i<contador-1; i++){
-            distanciaTotal += sqrt((float)(pow(p[i+1].x - p[i].x, 2) + pow(p[i+1].y - p[i].y, 2))); 
+            distanciaTotal += sqrt((float)(pow(ponto[i+1].x - ponto[i].x, 2) + pow(ponto[i+1].y - ponto[i].y, 2))); 
         }
-        distanciaAtalho = sqrt((float)(pow(p[contador-1].x - p[0].x, 2)) + (pow(p[contador-1].y - p[0].y, 2)));
         
-        bubbleSort(p, contador);
-        
-        
-        sprintf(saida, (contagemDeLinhas == 0) ? "points" : "\npoints");
-        while(c<contador){
-            sprintf(saida, "%s (%d,%d)", saida, p[c].x, p[c].y); //Essa linha pode ter ficado um pouco confusa, mas é só um while q tá concatenando as coordenadas com a saída anterior, acredito q vá funcionar
-            c++;
-        }
-        //Isso não tava batendo antes por que tem ser >>EXATAMENTE<< igual à saida do Dihego, contei até os espaços
-        sprintf(saida, "%s distance %.2f", saida, distanciaTotal);
-        sprintf(saida, "%s shortcut %.2f", saida, distanciaAtalho);
-        fputs(saida, fp_out);
+        bubbleSort(ponto, contador);     
+        criarSaida(fp_out, ponto, contador, distanciaTotal, distanciaAtalho, contagemDeLinhas);
         contagemDeLinhas++;
     }
     fclose(fp_in);
-    fclose(fp_out);    
+    fclose(fp_out);
+    printf("Tarefa concluida com sucesso!");    
     return 0;
 }
